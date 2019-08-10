b174851164de289e34effb754a98ab54a889d8d3
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-10 00:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 // basic math
 function add(a, b) {
-  
+  var add = (a + b)
+  return add
+}
+
+function subtract() {
+  var subtract = (a - b)
+  return subtract
+}
+
+function multiply() {
+  var multiply = (a * b)
+  return multiply
+}
+
+function divide() {
+  var divide = (a / b)
+  return divide
 }
